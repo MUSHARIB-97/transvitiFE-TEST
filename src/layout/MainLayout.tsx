8ac1ddef6c8bdc2d03e37af52f5aee48865831e6
MUSHARIB-97 <mushariburrab9@gmail.com>
2025-04-27 07:26:44 +0500

    changes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -8,10 +8,10 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   return (
     <main className="w-full h-screen overflow-x-hidden bg-[#F4F4F4]">
-      <section className="md:px-6 px-2 bg-white">
+      <header className="md:px-6 px-2 bg-white fixed top-0 left-0 right-0 z-[100]">
         <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
-      </section>
-      <section className="md:px-6 px-2 w-full">{children}</section>
+      </header>
+      <section className="md:px-6 px-2 w-full pt-[3.5rem]">{children}</section>
     </main>
   );
 };
